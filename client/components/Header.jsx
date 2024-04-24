73ce50bba4a23add4b9b20723ea2de8c30ebdf56
Brendan Xiong <32696639+brendanxiong@users.noreply.github.com>
2024-04-24 16:33:28 -0700

    edit landing page

--- client/components/Header.jsx
+++ client/components/Header.jsx
@@ -4,22 +4,24 @@ import '../stylesheets/header.css'
 const Header = () => {
 
   return (
-    <div className="header-container">
-      <div>
+    <div className="header-container flex">
+      <div className="logo">
         <Link to='/' className='text-white text-3xl font-bold font-serif text-center'>
           TRAVATAR</Link>
       </div>
-      <div className='text-right m-2'>
-        <Link to='/manager'>Manager</Link>
-      </div>
-      <div className='text-right m-2'>
-        <Link to='/about'>About</Link>
-      </div>
-      <div className='text-center m-2'>
-        <Link to='/register'>Register</Link>
-      </div>
-      <div className='text-center m-2'>
-        <Link to='/login'>Login</Link>
+      <div className="headerItems">
+        <div className='text-right m-2'>
+          <Link to='/manager'>Manager</Link>
+        </div>
+        <div className='text-right m-2'>
+          <Link to='/about'>About</Link>
+        </div>
+        <div className='text-center m-2'>
+          <Link to='/register'>Register</Link>
+        </div>
+        <div className='text-center m-2'>
+          <Link to='/login'>Login</Link>
+        </div>
       </div>
     </div>
   );
