9ac519a08c08009391b7afecd74950da87e593c7
Tdjergian <tdjerg@gmail.com>
2024-04-25 17:18:32 -0700

    working on testing...

--- server/controllers/userController.js
+++ server/controllers/userController.js
@@ -81,7 +81,8 @@ const getUser = async (req, res) => {
     res.status(500).json({ error: 'Internal server error'});
   }
   
-}
+};
+
 
 // generate json web token
 const generateToken = (id) => {
---
+++ tests/server/mongo/db.test.js
@@ -0,0 +1,46 @@
+// import { afterAll, afterEach, beforeAll, beforeEach, describe, expect } from 'vitest';
+const User = require('../../../server/models/User.js');
+const mongoose =  require('mongoose');
+const dotenv = require('dotenv');
+
+dotenv.config({ path: './config.env' });
+
+
+describe('adding a user', () => {
+  let user;
+  beforeAll( async () => {
+    console.log(process.env.MONGO_URI)
+    await mongoose.connect(`${process.env.MONGO_URI}`)
+  });
+
+  afterAll(() => {
+    mongoose.connection.close();
+  });
+
+  beforeEach(() => {
+    user = {
+      firstName: 'thisisafakefirstname',
+      lastName: 'thisisafakelastname',
+      email: 'fakeEmail@notReal.com6',
+      password: 'fakepassword'
+    };
+  });
+
+  afterEach( async () => {
+    await User.deleteOne({email: 'fakeEmail@notReal.com6'});
+  });
+
+  
+  it('should create a new user', async () => {
+    const newUser = await User.create(user);
+    expect(newUser.firstName).toBe(user.firstName);
+  });
+
+  it('should not create a user without a password', async () => {
+    // user.password = '';
+    const newUser = await User.create(user);
+    expect(newUser.password).toBe(user.password);
+  });
+
+  
+});
