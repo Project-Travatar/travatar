bab05c06c2be3ba70ca1db331a07a0f7d08139c5
Brendan Xiong <32696639+brendanxiong@users.noreply.github.com>
2024-05-02 19:20:44 -0700

    merge with Ashraf's image generator

--- client/components/Itinerary.jsx
+++ client/components/Itinerary.jsx
@@ -1,9 +1,24 @@
 import Reroll from './Reroll.jsx';
-
+import { createClient } from 'pexels';
 
 const Itinerary = ({ itinerary }) => {
   // console.log('jsx itin:', itinerary);
-  
+  document.backgroundDiv = 0;
+  const client = createClient('PEgQiisiCDMXUtU68bsOrQ6YvizFI0M1uhqWesivRNHbIOxdqtlEIRX2');
+  console.log('itinirary: ', JSON.stringify(itinerary));
+  let div = 0;
+  Object.entries(itinerary.itinerary).map(([date, timesOfDay]) => {
+    Object.entries(timesOfDay).map(([timeOfDay, suggestion]) => {
+      let query = suggestion.address;
+      client.photos.search({ query, per_page: 1 }).then(photos => {
+      client.photos.show({ id: photos.photos[Math.floor(Math.random() * photos.photos.length)].id}).then(photo => {
+      let picture = document.querySelector('#img'+ ++div) //.src = photo.src.original;
+      picture.src  = photo.src.original;
+        });
+      })
+    });
+  });
+
   if (itinerary) return (
     <div id='itinerary-details'>
       {Object.entries(itinerary.itinerary).map(([date, timesOfDay]) => (
@@ -16,12 +31,17 @@ const Itinerary = ({ itinerary }) => {
                   <h3 className="time-of-day">{timeOfDay}</h3>
                   <Reroll timeOfDay={timeOfDay} date={date} itinerary={itinerary.itinerary} />
                 </div>
-                <div className='activity='>
+                <div className='activity' style='float:left;width:50%'>
                 <p className='underline'>Activity: </p>
                 <p>{suggestion.activity}</p>
                 </div>
+                <div className='description'>
                 <p>Description: {suggestion.description}</p>
+                </div>
+                <div className='address'>
                 <p>Address: {suggestion.address}</p>
+                </div>
+                <img id={'img' + ++document.backgroundDiv} src='' alt='' width='100%'></img>
               </div>
             ))}
           </div>
